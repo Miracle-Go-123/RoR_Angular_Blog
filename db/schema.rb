0386e6788de7030ccaaeb4aab164c0531c89c6d6
Miracle-Go-123 <entsav.go@yahoo.com>
2016-03-18 00:01:30 -0600

    Added support for body in migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160318045743) do
+ActiveRecord::Schema.define(version: 20160318060045) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20160318045743) do
     t.integer  "user_id"
     t.string   "category"
     t.string   "image_url"
+    t.string   "body"
   end
 
   add_index "posts", ["user_id"], name: "index_posts_on_user_id", using: :btree
